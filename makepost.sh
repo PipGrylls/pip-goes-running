2308d820c2c1304e256eb9fdce17dcf17d05f4f5
PipGrylls <pipgryllsastro@gmail.com>
2023-08-02 08:32:41 +0100

    updating makepost.sh

--- makepost.sh
+++ makepost.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+# TODO: Write some webscraping scripts to auto update the goals/pbs and events section
+
 cd _posts || exit
 
 CURRENTDATE=$(date +"%Y-%m-%d")
@@ -16,4 +18,6 @@ echo "tags:" >> ${CURRENTDATE}-blog-post.md
 echo "  - weekly" >> ${CURRENTDATE}-blog-post.md
 echo "---" >> ${CURRENTDATE}-blog-post.md
 
+echo "{% include personal-bests.md %}" >> ${CURRENTDATE}-blog-post.md
 echo "{% include goals.md %}" >> ${CURRENTDATE}-blog-post.md
+echo "{% include upcoming-events.md %}" >> ${CURRENTDATE}-blog-post.md
